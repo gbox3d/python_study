a03c9d1efa48ba4c52d820f38675cacc254e8758
gbox3d <gbox3d@gmail.com>
2020-02-26 00:24:40 +0900

    time format

--- get_datetime.py
+++ get_datetime.py
@@ -5,6 +5,10 @@ now = datetime.now()
  
 print("now =", now)
 
+
+print("now time =" + now.strftime("%H%M"))
+
 # dd/mm/YY H:M:S
 dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
 print("date and time =", dt_string)	
+
