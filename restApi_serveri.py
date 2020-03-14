37d94ccfba420043fc6549d1fb6158e9987de03a
gbox3d <gbox3d@gmail.com>
2020-03-14 08:54:26 +0000

    rest sample update

--- restApi_serveri.py
+++ restApi_serveri.py
@@ -4,15 +4,24 @@
 # 참고자료 : https://m.blog.naver.com/PostView.nhn?blogId=wideeyed&logNo=221350669178&proxyReferer=https%3A%2F%2Fwww.google.com%2F
 # pip install flask
 
+import sys
 from flask import Flask, escape, request
+
+print(sys.version)
+
+_port=8000
+if len(sys.argv) > 1 : 
+    print(sys.argv[1])
+    _port = int(sys.argv[1])
 
 app = Flask(__name__)
 
 @app.route('/hello')
 def hello():
     name  = request.args.get("name")
-    return {"msg" : f'hello {name}'}
+    return {"msg" : 'hello' + name}
 
 if __name__ == '__main__':
-        app.run(host='0.0.0.0', port=8000, debug=True)
+    print('start port : ' + str(_port) )
+    app.run(host='0.0.0.0', port=_port, debug=True)
 
