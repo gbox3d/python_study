292e4774cf3d3824932bf5ffeed31c10c5f1a37e
gbox3d <gbox3d@gmail.com>
2020-03-16 15:05:35 +0900

    for 문 예제

---
+++ basic/for_exam.py
@@ -0,0 +1,10 @@
+for i in range(-5,5) : print(i)
+
+_list = [1,2,3,"hello"]
+
+for item in _list : print(item)
+
+#배열만들기 [표현식 forin 조건문]
+_arrfor = [ 10*x for x in range(1,10) if x%2 ]
+
+print(_arrfor)
