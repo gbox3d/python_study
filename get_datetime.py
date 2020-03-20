d13293ff2eac9c50aebf8570ba4876592821a95c
gbox3d <gbox3d@gmail.com>
2020-03-20 10:45:55 +0900

    ....

--- get_datetime.py
+++ get_datetime.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import time
 
 # datetime object containing current date and time
 now = datetime.now()
@@ -12,3 +13,8 @@ print("now time =" + now.strftime("%H%M"))
 dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
 print("date and time =", dt_string)	
 
+print(now.timetuple())
+#time stamp   변환 
+print(time.mktime(now.timetuple()))
+
+#참고 : https://inma.tistory.com/96
