0e8f417a2a4fc9f7b6e977674f45a192df2059b4
seokjun lee <gbox3d@MacBook-Pro.local>
2020-07-25 19:45:12 +0900

    .....

--- pandas/ex03_create.py
+++ pandas/ex03_create.py
@@ -6,4 +6,15 @@ import numpy as np
 
 #%%
 #data frame 만들기 
-df = pd.
+df = pd.DataFrame((np.random.rand(10,4) * 100).astype(np.int),
+    index=[ f'st_num{i}' for i in range(10) ],
+    columns=['kor','mat','eng','prg']
+
+)
+
+print(df)
+
+# %%
+df.to_csv('test.csv')
+
+# %%
