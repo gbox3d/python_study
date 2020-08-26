6829f8636a89c5c83b66cd1abf61cfb7938f51f7
seokjun lee <gbox3d@MacBook-Pro.local>
2020-08-26 15:37:55 +0900

    ...

--- network/tcp/server.py
+++ network/tcp/server.py
@@ -1,11 +1,18 @@
+#%%
 import socket
-server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-server_socket.bind("",8282)
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+server_socket.bind(("127.0.0.1",8282))
 server_socket.listen(5) # 5 CONNECTION QUEUE
 
+print('wait connect')
 client_socket ,addr = server_socket.accept()
-data = client_socket.recv(1024)
+print(f'client connected {addr}')
 
-print("recevied :" ,addr, data.decode())
+print('wait data')
+data = client_socket.recv(1024)
+print(f"recevied : {addr} {data.decode()}")
 
 client_socket.close()
+
+# %%
--- snippet/fileio_sample.py
+++ snippet/fileio_sample.py
@@ -1,6 +1,8 @@
+#%%
 import pathlib
 import os
 
+#%%
 filename = "test.txt"
 if os.path.isfile("test.txt") :
     print('file exist')
@@ -14,11 +16,24 @@ else :
     fd.write("hello python")
     fd.close()
 
+#%%
 #현제 실행경로 구하기
 print("current path (cwd) : ",os.getcwd())
 print("current path (pathlib) : ",pathlib.Path().absolute())
 
+#%%
+#파일 리스팅 
 filelist = os.listdir()
 # print(filelist)
 for _file in filelist : 
     print(_file)
+
+#%%
+import struct
+fd = open('test.bin',"wb")
+buf = bytes("hello python",'utf-8')
+
+fd.write(struct.pack('I',len(buf)))
+fd.write(buf)
+fd.close()
+# %%
