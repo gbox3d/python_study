71673ef6fa8bb34ee3b0654f37c4c26025e5fe06
gbox3d <gbox3d@gmail.com>
2020-10-29 02:53:16 +0000

    rest sample use default module

--- network/rest/tiny_server.py
+++ network/rest/tiny_server.py
@@ -1,13 +1,30 @@
 # %%
 import socket
 import json
+import time
 
-from http_parser.http import HttpStream
-from http_parser.reader import SocketReader
+# from http_parser.http import HttpStream
+# from http_parser.reader import SocketReader
+
 
 from urllib.parse import urlparse, parse_qs
 
 import argparse
+
+#https://stackoverflow.com/questions/4685217/parse-raw-http-headers
+from http.server import BaseHTTPRequestHandler
+from io import BytesIO
+
+class HTTPRequest(BaseHTTPRequestHandler):
+    def __init__(self, request_text):
+        self.rfile = BytesIO(request_text)
+        self.raw_requestline = self.rfile.readline()
+        self.error_code = self.error_message = None
+        self.parse_request()
+
+    def send_error(self, code, message):
+        self.error_code = code
+        self.error_message = message
 
 parser = argparse.ArgumentParser(description="argument parser sample")
 
@@ -20,49 +37,69 @@ port = 8282
 if _args.port:
     port = _args.port
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind(("", port))
-server_socket.listen(5)  # 5 CONNECTION QUEUE
 
 print(f'server bind {port}')
 
-while True:
-    print('wait connect')
-    client_socket, addr = server_socket.accept()
-    print(f'client connected {addr}')
+try:
 
-    print('wait data')
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind(("", port))
+    server_socket.listen(5)  # 5 CONNECTION QUEUE
 
-    r = SocketReader(client_socket)
-    p = HttpStream(r)
+    _loop = True
+    while _loop:
+        print('wait connect')
+        client_socket, addr = server_socket.accept()
+        print(f'client connected {addr}')
 
-    url = urlparse(p.url())
+        print('wait data')
+        data = client_socket.recv(1024)
+
+        print(data.decode())
+
+        req = HTTPRequest(data)
+        print(req.headers['Host'])
+        print(req.headers.keys())
+
+        print(f'method : {req.command}' )
+        print(f'path : {req.path}')
+        print(f'version : {req.request_version}')
+
+        url = urlparse(req.path)
+        print(url.path) # path
+
+        query = parse_qs(url.query) # 인자값 분석 
+        print(query)
+        
+
+        if url.path == '/exit' :
+            _loop = False
+        elif url.path == '/hello':
+            print(f"name : {query['name'][0]}")
+        else :
+            pass
 
 
-    print(url.path) # path
+        _str = 'HTTP/1.1 200 OK\r\n'
+        _str += 'Content-Type: Application/json\r\n'
+        #cors 관련 처리 
+        _str += 'Access-Control-Allow-Origin: *\r\n'
+        _str += 'Access-Control-Allow-Methods: GET\r\n'
+        _str += 'Access-Control-Max-Age: 1000\r\n\r\n'
+        _str += json.dumps({"r": "ok", "code": 0, "msg": "welcome"})
 
-    query = parse_qs(url.query) # 인자값 분석 
-    print(query)
-    print(f"name => {query['name'][0]}")
+        client_socket.sendall(_str.encode())
+        client_socket.close()
 
-    print(p.method())
-    print(p.headers())
-    print("Host =>" + p.headers()['Host'])
+except KeyboardInterrupt as ki:
+    # server_socket.close()
+    print('exit by keybord')
+except Exception as ex:
+    print(ex)
 
-    # print (p.body_file().read())
-
-    # data = client_socket.recv(1024)
-    # print(f"recevied : \r\n {data.decode()}")
-
-    _str = 'HTTP/1.1 200 OK\r\n'
-    _str += 'Content-Type: Application/json\r\n'
-    #cors 관련 처리 
-    _str += 'Access-Control-Allow-Origin: *\r\n'
-    _str += 'Access-Control-Allow-Methods: GET\r\n'
-    _str += 'Access-Control-Max-Age: 1000\r\n\r\n'
-    _str += json.dumps({"r": "ok", "code": 0, "msg": "welcome"})
-
-    client_socket.sendall(_str.encode())
-    client_socket.close()
+time.sleep(1)
+server_socket.close()
+time.sleep(1)
+print('bye~')
 
 # %%
