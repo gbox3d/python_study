c5838d1a47a96af3f89f9b927915a126070e58cd
gbox3d <gbox3d@gmail.com>
2020-12-19 21:58:26 +0900

    basic listt

---
+++ basic/ex03_list.py
@@ -0,0 +1,19 @@
+# To add a new cell, type '# %%'
+# To add a new markdown cell, type '# %% [markdown]'
+# %% [markdown]
+# list 선언하기
+
+# %%
+_list = (1,2,3,4)
+
+print(_list)
+
+
+# %%
+print(_list[1])
+
+
+# %%
+
+
+
