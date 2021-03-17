38f83a2964bc8bfaffa1358f35f004d34788f333
gbox3d <gbox3d@gmail.com>
2021-03-17 19:34:18 +0900

    post upload

--- network/rest/restApi_req.py
+++ network/rest/restApi_req.py
@@ -1,4 +1,4 @@
-#%%
+# %%
 import sys
 import requests
 import json
@@ -6,21 +6,26 @@ import json
 print(sys.version)
 
 
-#http://redstar001.iptime.org:17390/hello?name=gbox
+# %% get test
+# http://redstar001.iptime.org:17390/hello?name=gbox
 # url = "http://redstar001.iptime.org:17390/hello"
 url = "http://localhost:10048/hello"
 
 response = requests.get(url, params={
-    "name" : "gbox"
-}) 
+    "name": "gbox"
+})
 
-print("status code : ",response.status_code)
+print("status code : ", response.status_code)
 print("text : ", response.text)
 
 print(response.json()['msg'])
 
+# %% post sample
 
+response = requests.post('http://localhost:8282/upload',
+                         headers={'my-name': 'gbox3d'}, # headert
+                         data='hello' # body
+                         )
 
-
-
-# %%
+print("status code : ", response.status_code)
+print("text : ", response.text)
