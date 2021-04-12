f6d0b6d5945fd1763570c7c55ec8c0c87b3d4304
gbox3d <gbox3d@gmail.com>
2021-04-13 01:45:55 +0900

    ...

--- pytorch/ex01.py
+++ pytorch/ex01.py
@@ -1,7 +1,11 @@
 #%%
 import cv2
 import torch
+import torchvision
 import torch.backends.cudnn as cudnn
+
+print(f' cv version : {cv2.__version__}')
+print(f' torch version : {torch.__version__}')
 
 #%%
 _a = torch.Tensor([1,2,3])
