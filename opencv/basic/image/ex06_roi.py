f1659aced1f1c1ff691d035a04251ca6d4c29ea6
gbox3d <gbox3d@gmail.com>
2021-05-11 09:04:12 +0900

    ...

--- opencv/basic/image/ex06_roi.py
+++ opencv/basic/image/ex06_roi.py
@@ -1,10 +1,21 @@
-# region of images
+#%% region of images
 import numpy as np
 import cv2 as cv
 
-img = cv.imread('../res/messi5.jpg')
+from PIL import Image
+from IPython.display import display
+import pathlib
+
+#%%
+img = cv.imread('../../res/messi5.jpg')
+
+display(Image.fromarray(cv.cvtColor(img, cv.COLOR_BGR2RGB)))
+
+#%% crop and paste
 ball = img[280:340,330:390]
 img[273:333, 100:160] = ball
+display(Image.fromarray(cv.cvtColor(img, cv.COLOR_BGR2RGB)))
 
+#%%
 #save result
 cv.imwrite('../output/test.png',img)
