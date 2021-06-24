31986144c9fe58cc92fd2859a79b1f90dc2787dd
gbox3d <gbox3d@gmail.com>
2021-06-24 09:25:30 +0900

    ....

--- snippet/argparser.py
+++ snippet/argparser.py
@@ -1,11 +1,22 @@
 import argparse
 
+# 등호(=)는 생략가능하다. 된다.
+# python argparser.py -p=./test/tt1 -t=200
+# python argparser.py -p ./test/tt1 -t 200
+#
+
+
 parser = argparse.ArgumentParser(description="argument parser sample")
 
-parser.add_argument('--test', type=int, 
+parser.add_argument(
+    '-t','--test', type=int, 
     default=100,
+    help='help : it is test')
+parser.add_argument('-p','--test-path', type=str, 
+    default='./temp',
     help='help : it is test')
 
 _args = parser.parse_args()
 
 print(_args.test)
+print(_args.test_path) # 매개변수 이름에 있는 -는 변수접근시 _ 로 바뀐다.
