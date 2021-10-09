5d73f18bf65b9f82a8c677528f66ef5cf15ae71c
gbox3d <gbox3d@gmail.com>
2021-10-09 16:47:01 +0900

    ...

--- pytorch/cuda_test.py
+++
@@ -1,6 +0,0 @@
-#%% 에러 안나면 cuda 성공
-import torch
-a = torch.rand(3).to('cuda')
-# %%
-print(a)
-# %%
--- pytorch/ex00_gpu_check.py
+++ pytorch/ex00_gpu_check.py
@@ -9,4 +9,8 @@ for i in range(0,count):
     print(f'gpu {i} : {torch.cuda.get_device_name(i)}'  )
 print( f'gpu count : {torch.cuda.device_count()}')
 
+# %% 에러가 안나면 쿠다가 잘 잡힌것
+a = torch.rand(3).to('cuda')
+print(a)
+# 출력 결과 : tensor([0.2463, 0.6583, 0.1663], device='cuda:0')
 # %%
