ca54fee388f4f84e06e5205e9ac87e5efe622049
gbox3d <gbox3d@gmail.com>
2021-10-15 13:36:21 +0900

    ...

--- detectron2/download_sample.sh
+++ detectron2/download_sample.sh
@@ -3,3 +3,8 @@ wget http://images.cocodataset.org/val2017/000000439715.jpg -q -O ../res/input.j
 wget http://farm5.staticflickr.com/4040/4212024872_c8f721ab0a_z.jpg -q -O ../res/orange.jpg
 
 #download source https://cocodataset.org/#explore
+
+# download datastes
+wget https://github.com/TannerGilbert/Detectron2-Train-a-Instance-Segmentation-Model/raw/master/microcontroller_segmentation_data.zip
+wget https://github.com/matterport/Mask_RCNN/releases/download/v2.1/balloon_dataset.zip -O ../../datasets
+wget https://github.com/Tony607/detectron2_instance_segmentation_demo/releases/download/V0.1/data.zip -q -O ../../datasets/fruts_nuts.zip
--- detectron2/ex05_dataset.py
+++ detectron2/ex05_dataset.py
@@ -24,8 +24,8 @@ from detectron2.config import get_cfg
 from detectron2.data import DatasetCatalog, MetadataCatalog
 from detectron2.data.datasets import register_coco_instances
 
-from detectron2.utils.visualizer import Visualizer
-from detectron2.engine import DefaultPredictor
+from detectron2.utils.visualizer import Visualizer,ColorMode
+# from detectron2.engine import DefaultPredictor
 
 
 #%%
@@ -46,11 +46,49 @@ display( Image.fromarray(cv2.cvtColor(im,cv2.COLOR_BGR2RGB)))
 
 # %%
 _meta = MetadataCatalog.get("microcontroller_test")
+_meta.thing_colors=[(255,0,0),(0,255,0),(0,0,255),(255,255,0)]
 # print(_meta)
 print( f'type : {_meta.evaluator_type}' )
 print(f'image root : {_meta.image_root}')
 print(f'json file path : {_meta.json_file}')
 print(f'class list :  {_meta.thing_classes}')
 print(f'name : {_meta.name}')
+
+# %%
+_d = dataset_dicts[0]
+img = cv2.imread(_d["file_name"])
+visualizer = Visualizer(img[:, :, ::-1], 
+    metadata=_meta, 
+    instance_mode=ColorMode.SEGMENTATION, # thing_colors로 지정해준 값을 사용하기 
+    scale=1.0)
+out = visualizer.draw_dataset_dict(_d) # 데이터셋 그리기 
+display( Image.fromarray(out.get_image()))
+
+# %% 저장하기 
+encode_param=[int(cv2.IMWRITE_JPEG_QUALITY),90]
+_,_encodee_img = cv2.imencode('.jpg',cv2.cvtColor(out.get_image(),cv2.COLOR_BGR2RGB),encode_param)
+
+with open('./out.jpg','wb') as fd :
+    fd.write( _encodee_img )
+
+
+
+# %% 직접 로딩하기
+import json
+
+_dataset_dicts = []
+json_file = os.path.join("../../datasets/fruts_nuts", "trainval.json")
+
+with open(json_file) as f:
+    imgs_anns = json.load(f)
+
+for img_index,_imginfo in enumerate(imgs_anns['images']) :
+    record = {}
+    file_name = _imginfo['file_name']
+    image_id = _imginfo['id']
+    print(file_name)
+
+
+
 
 # %%
