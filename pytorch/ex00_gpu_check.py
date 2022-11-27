370a1b2caf2ae612ed72ffa5c9f5f07fbeb46dbc
gbox3d <gbox3d@gmail.com>
2022-11-28 02:25:44 +0900

    check m1 gpu

--- pytorch/ex00_gpu_check.py
+++ pytorch/ex00_gpu_check.py
@@ -14,3 +14,9 @@ a = torch.rand(3).to('cuda')
 print(a)
 # 출력 결과 : tensor([0.2463, 0.6583, 0.1663], device='cuda:0')
 # %%
+print (f"PyTorch version:{torch.__version__}") # 1.12.1 이상
+print(f"MPS 장치를 지원하도록 build 되었는지: {torch.backends.mps.is_built()}") # True 여야 합니다.
+print(f"MPS 장치가 사용 가능한지: {torch.backends.mps.is_available()}") # True 여야 합니다.
+
+#%%
+# device = torch.device('mps:0' if torch.backends.mps.is_available() else 'cpu')
