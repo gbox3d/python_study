a178e864a084d21b1edbf4dfcf901a1ab5967a95
gbox3d <gbox3d@gmail.com>
2023-07-10 17:52:05 +0900

    .....

--- pytorch/ex00_gpu_check.py
+++ pytorch/ex00_gpu_check.py
@@ -2,8 +2,10 @@
 import torch
 print(torch.__version__)
 print(f'cuda is available {torch.cuda.is_available()}')
+
 #%%
 # torch.rand(3).to('cuda')
+print(f"cuda version : {torch.version.cuda}") # 10.2 이상
 count = torch.cuda.device_count()
 for i in range(0,count):
     print(f'gpu {i} : {torch.cuda.get_device_name(i)}'  )
@@ -17,6 +19,10 @@ print(a)
 print (f"PyTorch version:{torch.__version__}") # 1.12.1 이상
 print(f"MPS 장치를 지원하도록 build 되었는지: {torch.backends.mps.is_built()}") # True 여야 합니다.
 print(f"MPS 장치가 사용 가능한지: {torch.backends.mps.is_available()}") # True 여야 합니다.
+
+#%%
+print(f"GPU 장치를 지원하도록 build 되었는지: {torch.backends.cuda.is_built()}") # True 여야 합니다.
+
 
 #%%
 # device = torch.device('mps:0' if torch.backends.mps.is_available() else 'cpu')
