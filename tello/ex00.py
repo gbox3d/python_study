5ecaadef4c0b493cc9af549d2d7592f3019c449e
gbox3d <gbox3d@gmail.com>
2025-02-26 22:07:55 +0900

    tello

--- tello/ex00.py
+++ tello/ex00.py
@@ -1,11 +1,24 @@
-#%%
 import socket
+import time
 
+# 1) 소켓 먼저 생성해 상태를 받을 준비
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind(('', 8890))
 
+# 2) Tello에 command 모드 보내기
+tello_addr = ('192.168.10.1', 8889)
+sock.sendto(b'command', tello_addr)
+
+# 3) Tello 응답 확인(= 'ok' 수신)
+sock.settimeout(3)
+try:
+    data, addr = sock.recvfrom(1024)
+    print("Command response:", data)
+except socket.timeout:
+    print("No response for 'command'")
+
+# 4) 이후부터 상태 패킷을 기다림
+sock.settimeout(None)
 while True:
     data, addr = sock.recvfrom(1024)
     print("Received:", data, "from", addr)
-
-# %%
