2a510679090f0160fe421720c576e7f24eab5a82
gbox3d <gbox3d@gmail.com>
2025-10-04 17:31:05 +0900

    torch 예제uv 로 변경

--- pytorch_study/ex00_gpu_check.py
+++ pytorch_study/ex00_gpu_check.py
@@ -7,9 +7,12 @@ print(f'cuda is available {torch.cuda.is_available()}')
 # torch.rand(3).to('cuda')
 print(f"cuda version : {torch.version.cuda}") # 10.2 이상
 count = torch.cuda.device_count()
-for i in range(0,count):
-    print(f'gpu {i} : {torch.cuda.get_device_name(i)}'  )
 print( f'gpu count : {torch.cuda.device_count()}')
+
+for i in range(count):
+    name = torch.cuda.get_device_name(i)
+    mem_gb = torch.cuda.get_device_properties(i).total_memory / 1024**3
+    print(f"gpu {i} : {name} memory: {mem_gb:.2f} GB")
 
 # %% 에러가 안나면 쿠다가 잘 잡힌것
 a = torch.rand(3).to('cuda')
@@ -22,6 +25,7 @@ print(f"MPS 장치가 사용 가능한지: {torch.backends.mps.is_available()}")
 
 #%%
 print(f"GPU 장치를 지원하도록 build 되었는지: {torch.backends.cuda.is_built()}") # True 여야 합니다.
+
 
 
 #%%
